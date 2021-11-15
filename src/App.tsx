7a06045fd8f5016493a6ca715e4087865db5581f
gytks4 <gytks4@naver.com>
2021-11-15 17:25:04 +0900

    swr 예시 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,56 @@
+import { client, fetcher } from "libs/api";
+import useSWR from "swr";
+import styled from "styled-components";
+import Articles from "Articles";
+
+export interface IArticles {
+  id: number;
+  title: string;
+}
+
+export interface ArticleSWRResponse<T> {
+  status: number;
+  data: T;
+}
+
 function App() {
-  return <div></div>;
+  const { data, error, mutate } = useSWR<ArticleSWRResponse<IArticles[]>>(
+    "/article",
+    fetcher,
+    {
+      refreshInterval: 100,
+    }
+  );
+  const articleData = data?.data;
+
+  const handlePost = async () => {
+    if (!articleData) return;
+    const id = articleData.length + 1;
+    await client.post("article", { id, title: `${id}번째 게시물` });
+  };
+
+  const handleMutate = () => {
+    mutate();
+  };
+
+  return (
+    <StyledRoot>
+      <Articles />
+      <StyledCenterLine />
+      {articleData &&
+        articleData.map((li) => <div key={li.id}>{li.title}</div>)}
+      <button onClick={handlePost}>post button</button>
+      <button onClick={handleMutate}>mutate button</button>
+    </StyledRoot>
+  );
 }
 
 export default App;
+
+const StyledRoot = styled.div``;
+
+const StyledCenterLine = styled.div`
+  width: 100%;
+  height: 2px;
+  background-color: black;
+`;
--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,1 +1,10 @@
-export const fetcher = (url: string) => fetch(url).then((res) => res.json());
+import axios from "axios";
+
+export const client = axios.create({
+  baseURL: "http://localhost:4000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const fetcher = (url: string) => client.get(url);
